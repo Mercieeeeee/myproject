3ff9355d0b98666060369dfcad0a61ff8367bd93
Mercieeeeee <mixtheory587@gmail.com>
2019-03-24 01:48:57 +0800

    Initial Commit

---
+++ src/main/java/com/Mercieeeeee/Hello.java
@@ -0,0 +1,22 @@
+package com.Mercieeeeee;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+        Person p = new Person();
+        p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float wetght = 66.5f;
+//        Float;
+        double heught = 1.7;
+//        Double;
+        boolean adult = true;
+        boolean enroll = false;
+//        Boolean;
+        String name = "Mercieeeeee";
+    }
+}
---
+++ src/main/kotlin/com/Mercieeeeee/Hello.kt
@@ -0,0 +1,20 @@
+package com.Mercieeeeee
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//        Human().hello()
+        var age = 19
+        age = 20
+        var weught : Float = 66.5f
+        var name : String
+        name = "Mercieeeeee"
+
+
+}
+
+class Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+
+}
