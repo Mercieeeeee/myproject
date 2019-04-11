a99ac7ff614e050972f8a2f49c5f0f522be948aa
Mercieeeeee <mixtheory587@gmail.com>
2019-04-11 13:54:55 +0800

    Initial Date

--- src/main/java/com/Mercieeeeee/Person.java
+++ src/main/java/com/Mercieeeeee/Person.java
@@ -4,6 +4,7 @@ public class Person{
     String name;
     float weight;
     float height;
+
     public Person(float weight, float height){
         this.weight = weight;
         this.height = height;
