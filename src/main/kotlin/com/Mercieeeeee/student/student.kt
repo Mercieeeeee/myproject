6ab234182b261594b05bfb6bc62229f7bf452032
Mercieeeeee <mixtheory587@gmail.com>
2019-04-21 01:03:56 +0800

    Initial switch

--- src/main/kotlin/com/Mercieeeeee/student/student.kt
+++ src/main/kotlin/com/Mercieeeeee/student/student.kt
@@ -1,13 +1,59 @@
-package com.Mercieeeeee.student
+package com.Kotlin.student
 
-fun main(args: Array<String>) {
-    val stu = Student("Mercieeeeee", 66, 88)
+import java.util.*
+
+fun main() {
+//    userInput()
+    val stu = Student("Mercieeeeee", 77, 99)
     stu.print()
+    val test = 123
+    println("Test is $test")
+    println("High score ${stu.highest()}")
 }
 
-class Student(var name: String, var english: Int, var math: Int) {
+private fun userInput() {
+    val scanner = Scanner(System.`in`)
+    println("Please enter student's name: ")
+//    var name = scanner.next()
+    var name = null
+    println("Please enter student's english: ")
+    var english = scanner.nextInt()
+    println("Please enter student's math: ")
+    var math = scanner.nextInt()
+    val stu = Student(name, english, math)
+    stu.print()
+    stu.nameCheck()
+}
+
+class Student(var name: String?, var english: Int, var math: Int) {
     fun print() {
-        println(name + "\t" + english + "\t" + math + "\t" +
-                (english+math)/2)
+        println(name + "\t" + english + "\t" + math + "\t"
+                + getAverage() + "\t" +
+                if (getAverage() >= 60) "PASS" else "FAILED")
+    }
+
+    fun getAverage() : Int {
+        return (english + math) / 2
+    }
+
+    fun highest() : Int {
+        var max = if (english > math) {
+            println("english")
+            english
+        } else {
+            println("math")
+            math
+        }
+//        var max = 0
+        /*if (english > math) {
+            max = english
+        } else {
+            max = math
+        }*/
+        return max
+    }
+
+    fun nameCheck() {
+        println(name?.length)
     }
 }
